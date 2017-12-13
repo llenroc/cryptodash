ecd02d5c15c01b2a99af41dbdf2e9f8484e9befd
Coinio <coinio@users.noreply.github.com>
2017-12-13 21:15:17 +0000

    Basic layout

--- js/app.js
+++ js/app.js
@@ -5,11 +5,13 @@ let app = new Vue({
 
     el: "#app",
     data: { 
-        appName: 'crypto-dash',
+        appName: 'Crypto.Dash',
+        marketData: {},
         top10: [],
         coinImgCache: []
     },    
     created: function() {        
+        this.getMarket();
         this.getCoinDetails();
         this.getTop10();
     },     
@@ -28,11 +30,30 @@ let app = new Vue({
                 });
         },
 
+        // Retrieve the global market data
+        getMarket: function () {
+            axios.get('https://api.coinmarketcap.com/v1/global')
+                .then((response) => {
+                    this.marketData = response.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+
         // Retrieve the top 10 images for the coins
         getCoinDetails: function() {
             this.coinImgCache.push({ symbol: 'BTC', url: 'https://www.cryptocompare.com/media/19633/btc.png' });
             this.coinImgCache.push({ symbol: 'ETH', url: 'https://www.cryptocompare.com/media/20646/eth_logo.png'});
             this.coinImgCache.push({ symbol: 'XRP', url: 'https://www.cryptocompare.com/media/19972/ripple.png'});
+            this.coinImgCache.push({ symbol: 'MIOTA', url: 'https://www.cryptocompare.com/media/1383540/iota_logo.png'});
+            this.coinImgCache.push({ symbol: 'LTC', url: 'https://www.cryptocompare.com/media/19782/litecoin-logo.png'});
+            this.coinImgCache.push({ symbol: 'DASH', url: 'https://www.cryptocompare.com/media/20626/imageedit_27_4355944719.png'});
+            this.coinImgCache.push({ symbol: 'XEM', url: 'https://www.cryptocompare.com/media/20490/xem.png'});
+            this.coinImgCache.push({ symbol: 'BTG', url: 'https://www.cryptocompare.com/media/12318377/btg.png'});
+            this.coinImgCache.push({ symbol: 'XMR', url: 'https://www.cryptocompare.com/media/19969/xmr.png'});
+            this.coinImgCache.push({ symbol: 'BCH', url: 'https://www.cryptocompare.com/media/1383919/bch.jpg'});
+
         },
 
         // Tidy this shit up. Lambda equivs?
