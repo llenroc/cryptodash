a649d292b4f46689d63759b721ae09c9d6eb173b
Coinio <coinio@users.noreply.github.com>
2017-12-26 13:32:38 +0000

    Saving progress before refactor

--- js/app.js
+++ js/app.js
@@ -6,30 +6,96 @@ let app = new Vue({
     el: "#app",
     data: { 
         appName: 'Crypto.Dash',
+        quoteCurrency: 'USD', 
         marketData: {},
         top10: [],
         coinImgCache: []
     },    
-    created: function() {        
-        this.getMarket();
-        this.getCoinDetails();
-        this.getTop10();
+    created: function() { 
+
+        this.loadDataSources();    
+
     },     
+    ready: function() {
+        
+    },
     methods: {
 
-        // Retrieve the top 10 coins by value
-        getTop10: function () {       
-           
+        loadDataSources: function() {
             axios.get('https://api.coinmarketcap.com/v1/ticker/?limit=10')
-                .then((response) => {
-                    this.top10 = response.data;               
-                    this.setCoinImages(this.top10);
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
+            .then((response) => {
+                this.top10 = this.getCoinsViewModel(response.data);         
+                this.getCoinDetails();      
+                this.setCoinImages(this.top10);
+                this.subscribeToUpdates(this.top10, this.quoteCurrency);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
         },
 
+        getCoinsViewModel: function(top10fromApi) {
+
+            var coins = [];
+
+            for(var index in top10fromApi)
+            {
+                coins.push({
+                    name: top10fromApi[index].name,
+                    symbol: top10fromApi[index].symbol,
+                    price: top10fromApi[index].price_usd,
+                    percentChanged: top10fromApi[index].percent_change_24h
+                });                
+            }
+
+            return coins;
+        },
+        updateCoin: function(symbol, newPrice, open24hour) {
+            
+            if(typeof(newPrice) == 'undefined')
+                return; 
+
+            var coin = this.top10.find(function(item) { return item.symbol === symbol; });
+
+            if(typeof(coin) == 'undefined')
+                return;
+
+            coin.price = newPrice;
+
+            if(typeof(open24hour) != 'undefined') {
+                var percentChanged = ((newPrice - open24hour) /  open24hour * 100).toFixed(2);
+                coin.percentChanged = percentChanged;
+            }
+        },
+
+        // Subscribe to the market price updates for the top 10 coins
+        subscribeToUpdates: function (coins, currency) {
+
+            var subscriptions = [];
+
+            for (var index in coins)
+            {
+                subscriptions.push('5~CCCAGG~' + this.getCoinAlias(coins[index].symbol) + '~' + currency);
+            }
+
+            var socket = io.connect('https://streamer.cryptocompare.com/');
+    
+            socket.emit('SubAdd', { subs: subscriptions });
+    
+            socket.on("m", (message) => {
+                var messageType = message.substring(0, message.indexOf("~"));
+                
+                var result = {};
+    
+                if(messageType === CCC.STATIC.TYPE.CURRENTAGG) {
+                    result = CCC.CURRENT.unpack(message);
+                    this.updateCoin(result.FROMSYMBOL, result.PRICE, result.OPEN24HOUR);
+                    console.log(result);
+                   // this.top10.sort((a,b) => { return a.-b; });
+                }
+    
+            });
+        },
         // Retrieve the global market data
         getMarket: function () {
             axios.get('https://api.coinmarketcap.com/v1/global')
@@ -67,21 +133,20 @@ let app = new Vue({
                     }
                 }
             }
+        },
+
+        getCoinAlias: function(symbol) {
+            if(symbol === 'MIOTA') 
+                return 'IOT';
+
+            return symbol;
         }
     }
 });
 
-
-           /* axios.get('https://www.cryptocompare.com/api/data/coinlist/', {      
-                method: 'GET',
-                mode: 'no-cors',
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Content-Type': 'application/json',
-                }})
-                .then((response) => {
-                    this.coinCache = response.data.Data;
-                })
-                .catch((error) => {
-                    console.log(error);
-                });*/
+// Refresh the pages data sources periodically to pull the new top 10 coins from the coin market cap api
+// Feels a little hacky
+// This will refresh all prices at once
+setInterval(() => {
+    app.loadDataSources();
+}, 5 * 60 * 1000);
